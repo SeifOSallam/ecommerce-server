e6927b44ced29f1851370d915e5d03b7dd3251dc
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-04-01 13:51:14 +0200

    add the avg_rate and total_rate to the fields of product

--- products/serializer.py
+++ products/serializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Product, Image
+from rate.models import Rate
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,12 +9,31 @@ class ImageSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(serializers.ModelSerializer):
     images = ImageSerializer(many=True, read_only=True)
+    
 
-    # def validate(self, data):
-    #     if data['price'] > 10:
-    #         raise serializers.ValidationError("Price must be positive")
-    #     return data
+    average_rate = serializers.SerializerMethodField()
+    total_rates = serializers.SerializerMethodField()
+    
+    def get_total_rates(self, obj): 
+        rates = Rate.objects.filter(product=obj)
+        
+        total_rates = rates.count()
+        
+        return total_rates
+    
+    def get_average_rate(self, obj):
+        rates = Rate.objects.filter(product=obj)
+        
+        total_rates = rates.count()
+        sum_of_rates = sum(rate.rate for rate in rates)
+
+        if total_rates > 0:
+            average_rate = sum_of_rates / total_rates
+        else:
+            average_rate = 0
+        
+        return average_rate
 
     class Meta:
         model = Product
-        fields = ('id', 'name', 'description', 'price', 'stock', 'category', 'images')
+        fields = ('id', 'name', 'description', 'price', 'stock', 'category', 'images', 'average_rate', 'total_rates')
