554744592d9b18a02960c70dc6712711f2dcde31
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-04-09 20:52:37 +0200

    update the cart serializer to return image of each product

--- cart/serializer.py
+++ cart/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Cart, CartItem
 from products.serializer import ProductSerializer
+from products.models import Image
+from products.serializer import ImageSerializer
 
 class CartItemSerializer(serializers.ModelSerializer):
 
@@ -11,8 +13,10 @@ class CartItemSerializer(serializers.ModelSerializer):
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         product_data = instance.product
-        product_serializer = ProductSerializer(product_data)
-        representation['product'] = product_serializer.data
+        product_serializer = ProductSerializer(product_data).data
+        image = Image.objects.filter(product=product_data.pk).first()
+        representation['image'] = ImageSerializer(image).data if image else None
+        representation['product'] = product_serializer
         return representation
 
 
