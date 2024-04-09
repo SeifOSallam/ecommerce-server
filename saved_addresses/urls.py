77eb6c3423de6d3be9c4a6bbe0734aaaa54b662d
Yousef Elsayed <yousef4511@outlook.com>
2024-04-09 14:52:33 +0200

    Fix: Restrict users to only CRUD their records

--- saved_addresses/urls.py
+++ saved_addresses/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
+from . import views
 
 router = DefaultRouter()
-router.register('',views.SavedAddressesViewSet)
+router.register("", views.SavedAddressesViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-
+    path("", include(router.urls)),
 ]
--- saved_addresses/views.py
+++ saved_addresses/views.py
@@ -1,14 +1,38 @@
-from django.shortcuts import render
-from rest_framework.decorators import api_view,action
+from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.response import Response
-from rest_framework import status,viewsets,filters
-from .serializer import SavedAddressesSerializer
+
 from .models import SavedAddresses
+from .serializer import SavedAddressesSerializer
+
 
 class SavedAddressesViewSet(viewsets.ModelViewSet):
     serializer_class = SavedAddressesSerializer
     queryset = SavedAddresses.objects.all()
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset()).filter(user=request.user)
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
     def perform_create(self, serializer):
-        print(self.request.user)
         serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        instance = serializer.instance
+        if instance.user == self.request.user:
+            serializer.save()
+        else:
+            raise PermissionDenied("You are not allowed to update this record.")
+
+    def perform_destroy(self, instance):
+        if instance.user == self.request.user:
+            instance.delete()
+        else:
+            raise PermissionDenied("You are not allowed to delete this record.")
