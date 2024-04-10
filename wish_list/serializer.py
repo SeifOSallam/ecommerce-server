b12a3fd553c20b6553c1f531f1baada90f5e41ca
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-04-10 22:21:42 +0200

    add the wishlist model and serializer

--- wish_list/serializer.py
+++ wish_list/serializer.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from .models import WishList
 from products.serializer import ProductSerializer
 from products.models import Product
-
+from products.serializer import ProductSerializer
+from products.models import Image
+from products.serializer import ImageSerializer
 
 class WishListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,5 +17,9 @@ class WishListSerializer(serializers.ModelSerializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
+        product_data = instance.product
+        image = Image.objects.filter(product=product_data.pk).first()
+        representation['image'] = ImageSerializer(image).data.get('image_url') if image else None
         representation['product'] = ProductSerializer(instance.product).data
         return representation
+    
