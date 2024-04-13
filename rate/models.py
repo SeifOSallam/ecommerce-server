e3d78e0b261c5bdca8afb41162a0ea7facccd4e2
SeifOSallam <seif.osallam@gmail.com>
2024-04-13 21:15:04 +0200

    meta added

--- rate/models.py
+++ rate/models.py
@@ -7,3 +7,6 @@ class Rate(models.Model):
     product = models.ForeignKey(Product, on_delete = models.CASCADE, null=False)
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     rate = models.PositiveIntegerField(validators = [MaxValueValidator(5)])
+
+    class Meta():
+        unique_together = (('user', 'product'))
